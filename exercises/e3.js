f5157e3e8d51a5f1f30b66f33a6fadc2d3785f51
Prince Mwabuponde <mwabupondep@gmail.com>
2024-03-03 12:12:53 -0500

    completed e3

--- exercises/e3.js
+++ exercises/e3.js
@@ -14,7 +14,21 @@
  * doesArrayIncludeItemsBetweenVals([2, 4, 2], 5, 10) => false
  */
 
-// Your code goes here...
+function doesArrayIncludeItemsBetweenVals(arr, val1, val2) {
+  for (let i = 0; i < arr.length; i++) {
+    const currentItem = arr[i];
+
+    if (currentItem > val1 && currentItem < val2) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+console.log(doesArrayIncludeItemsBetweenVals([2, 4, 2], 3, 5)); 
+console.log(doesArrayIncludeItemsBetweenVals([2, 4, 2], 5, 10)); 
+
 
 
 
@@ -34,8 +48,38 @@
  * getValueWithConditionOne(20, 30) => 100;
  */
 
-// Your code goes here...
+function getValueWithConditionOne(num1, num2) {
+  if (num1 === 40 && num2 === 40) {
+    return num1 + num2;
+  } else {
+    return (num1 + num2) * 2;
+  }
+}
 
+console.log(getValueWithConditionOne(40, 40));
+console.log(getValueWithConditionOne(20, 30));
+
+const getValueWithConditionTwo = (num1, num2) => {
+  if (num1 === 40 && num2 === 40) {
+    return num1 + num2;
+  } else {
+    return (num1 + num2) * 2;
+  }
+};
+
+console.log(getValueWithConditionTwo(40, 40));
+console.log(getValueWithConditionTwo(20, 30));
+
+const getValueWithConditionThree = function(num1, num2) {
+  if (num1 === 40 && num2 === 40) {
+    return num1 + num2;
+  } else {
+    return (num1 + num2) * 2;
+  }
+};
+
+console.log(getValueWithConditionThree(40, 40));
+console.log(getValueWithConditionThree(20, 30)); 
 
 
 
